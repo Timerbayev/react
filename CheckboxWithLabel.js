ecbe87534c50794cafd4fb881de386a1a7627e34
Aidar <account_service@bk.ru>
2018-11-22 21:46:47 +0300

    react 22.11.2018 21.46

--- CheckboxWithLabel.js
+++ CheckboxWithLabel.js
@@ -1,21 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default class CheckboxWithLabel extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {isCheckboxOn:false};
-        this.handleClick = this.handleClick.bind(this);
-    }
-    handleClick(){this.setState(prevState => ({isCheckboxOn:!prevState.isCheckboxOn}));}
+        this.state = {value: false};
+        this.labelOn = 'On';
+        this.labelOff = 'Off';
+        this.HandleClick = this.HandleClick.bind(this)
+        this.HandleChange = this.HandleChange.bind(this)
 
-        render() {
-            return(
-                <label htmlFor="name">
-                    {this.state.isCheckboxOn?'labelOn':'labelOff'}
-                    <input type ="checkbox" id = "name" onClick={this.handleClick}/>
-                </label>
-            )}
     }
-   
+    HandleClick = () => {
+        };
+    HandleChange = () => {
+        this.setState({value:!this.state.value})};
+
+    render() {
+
+        return (
+            <form onClick={this.HandleClick}>
+             <label htmlFor="name">
+                 {this.state.value ? this.labelOn : this.labelOff}
+                <input type="checkbox" onChange={this.HandleChange}
+                       value = {this.state.value} id="name"/>
+
+            </label>
+            </form>)
+    }
+}
+
 
